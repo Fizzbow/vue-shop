d1083ab5fce42329decfbc0daaf191cd1808d649
Fizzbow <541375716@qq.com>
2021-10-28 12:36:19 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
+import './plugins/element.js'
+
+import './assets/assets/fonts/iconfont.css'
+
+
+ import './assets/assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://139.9.202.95:8888/api/private/v1/login'
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
